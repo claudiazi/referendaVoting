a3237e8983dc8b75f34209ecadcd1e9df8305618
claudiazi <claudiazi1244@gmail.com>
2022-02-27 21:56:37 +0100

    Split the first chart to two & add filter

--- data_preparation.py
+++ data_preparation.py
@@ -15,10 +15,10 @@ def load_data(mongodb_url: str, db_name: str, table_name: str) -> pd.DataFrame:
 def preprocessing(df: pd.DataFrame):
     df = df.explode("votes")
     df = pd.concat([df, df.votes.apply(pd.Series)], axis=1)
-    df["balance"] = pd.to_numeric(df["balance"]).apply(lambda x: x / 1000000000)
-    df["time"] = df["time"].apply(
-        lambda x: datetime.datetime.fromtimestamp(x / 1000).strftime(
-            "%Y-%m-%d %H:%M:%S"
-        )
-    )
+    df["totalIssuance"] = df["totalIssuance"].astype(str).astype(float)
+    df["balance"] = df["balance"].astype(str).astype(float)
+    df["id"] = df["id"].astype(int)
+    # df["balance"] = pd.to_numeric(df["balance"]).apply(lambda x: x / 1000000000)
+    df["time"] = pd.to_datetime(df["time"], unit="ms")
+    df = df.drop("votes", axis=1)
     return df
--- main.py
+++ main.py
@@ -25,29 +25,50 @@ votes_df = load_data(mongodb_url=mongodb_url, db_name=db_name, table_name=table_
 votes_df = preprocessing(votes_df)
 
 # Sidebar - Referendum selection
-sorted_referendum_id = sorted(votes_df.id.unique())
-selected_id = st.sidebar.multiselect(
-    "Referendum ID", sorted_referendum_id, sorted_referendum_id
-)
+referendum_ids = sorted(votes_df.id.unique())
+max_id = int(max(referendum_ids))
+min_id = int(min(referendum_ids))
+selected_ids = st.sidebar.slider("Select a range of ids", min_value=min_id, max_value=max_id, value=[min_id, max_id], step=1)
+votes_df = votes_df[(votes_df["id"] >= selected_ids[0]) & (votes_df["id"] <= selected_ids[1])]
 
-df_balance_sum = (
-    votes_df.groupby("id")["balance"]
-    .sum()
-    .reset_index(name="balance_sum")
-    .sort_values(by="id")
-)
+# First Chart
 df_count_sum = (
-    votes_df.groupby("id")["accountId"]
+    votes_df.groupby(["time", "id"])["accountId"]
     .count()
     .reset_index(name="vote_counts")
-    .sort_values(by="id")
+    .sort_values(by="time")
 )
-df_sum = pd.merge(df_balance_sum, df_count_sum)
+
 with sns.axes_style("white"):
-    fig, ax = plt.subplots(figsize=(7, 5))
-    ax1 = sns.barplot(x="id", y="balance_sum", data=df_sum)
-    ax2 = plt.twinx()
-    sns.barplot(x="id", y="vote_counts", data=df_sum, ax=ax2)
+    fig, ax = plt.subplots(figsize=(7, 4))
+    sns.barplot(x=df_count_sum.time.dt.strftime('%Y-%m-%d'), y="vote_counts", data=df_count_sum, color="salmon")
+    ax.set(xlabel='Time (referendum closed)', ylabel='Counts of votes')
+    ax.xaxis.set_major_locator(plt.MaxNLocator(3))
+st.pyplot(fig)
+
+# Second chart
+df_balance_sum = (
+    votes_df.groupby(["time", "id"])["balance"]
+    .sum()
+    .reset_index(name="balance_sum")
+    .sort_values(by="time")
+)
+
+df_total_issuance_sum = (
+    votes_df.groupby(["time", "id"])["totalIssuance"]
+    .mean()
+    .reset_index(name="total_issuance")
+    .sort_values(by="time")
+)
+
+df_votes_balance_perc = df_balance_sum.merge(df_total_issuance_sum, how="inner", on=["id", "time"])
+df_votes_balance_perc["perc"] = df_votes_balance_perc["balance_sum"] / df_votes_balance_perc["total_issuance"] * 100
+
+with sns.axes_style("white"):
+    fig, ax = plt.subplots(figsize=(7, 4))
+    sns.lineplot(x=df_votes_balance_perc.time.dt.strftime('%Y-%m-%d'), y="perc", data=df_votes_balance_perc)
+    ax.set(xlabel='Time (referendum closed)', ylabel='Turnout (% of total Kusama voted)')
+    ax.xaxis.set_major_locator(plt.MaxNLocator(3))
 st.pyplot(fig)
 
 df_higest_balance_user = votes_df.groupby("accountId")["balance"].sum().reset_index()
