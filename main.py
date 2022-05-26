f7bfb87468110aa3089b5be986c59f78b56de72c
claudiazi <claudiazi1244@gmail.com>
2022-05-26 15:20:19 +0200

    Add the fifth graph

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ import plotly.express as px
 
 from data_preparation import load_data, preprocessing
 
+st.set_page_config(layout="wide")
+
 st.title("Referendum Voting")
 
 st.markdown(
@@ -208,8 +210,8 @@ third_graph_data = [
         line=dict(color="rgb(0, 0, 100)"),
         marker=dict(color="rgb(0, 0, 100)", size=8),
         hovertemplate="Referendum id: %{x:.0f}<br>"
-                      + "% of total votes counts: %{y:.4f}<br>"
-                      + "<extra></extra>",
+        + "% of total votes counts: %{y:.4f}<br>"
+        + "<extra></extra>",
     ),
 ]
 
@@ -232,6 +234,157 @@ third_graph_layout = go.Layout(
 
 fig_third_graph = go.Figure(data=third_graph_data, layout=third_graph_layout)
 st.plotly_chart(fig_third_graph)
+
+
+## Forth part
+df_conviction_mean = votes_df[["id", "conviction"]].groupby("id").mean()
+df_conviction_median = votes_df[["id", "conviction"]].groupby("id").median()
+# df_conviction = pd.merge(df_conviction_mean, df_conviction_median, on='id')
+
+df_voted_ksm_mean = votes_df[["id", "voted_ksm"]].groupby("id").mean()
+df_voted_ksm_median = votes_df[["id", "voted_ksm"]].groupby("id").median()
+
+forth_graph_data_1 = [
+    go.Scatter(
+        name="mean",
+        x=df_conviction_mean.index,
+        y=df_conviction_mean["conviction"],
+        mode="lines+markers",
+        line=dict(color="rgb(0, 0, 100)"),
+        marker=dict(color="rgb(0, 0, 100)", size=8),
+        hovertemplate="Referendum id: %{x:.0f}<br>"
+        + "conviction mean: %{y:.4f}<br>"
+        + "<extra></extra>",
+    ),
+    go.Scatter(
+        name="median",
+        x=df_conviction_median.index,
+        y=df_conviction_median["conviction"],
+        mode="lines+markers",
+        line=dict(color="rgb(0, 200, 200)"),
+        marker=dict(color="rgb(0, 200, 200)", size=8),
+        hovertemplate="Referendum id: %{x:.0f}<br>"
+        + "conviction median: %{y:.4f}<br>"
+        + "<extra></extra>",
+    ),
+]
+
+forth_graph_data_2 = [
+    go.Scatter(
+        name="mean",
+        x=df_voted_ksm_mean.index,
+        y=df_voted_ksm_mean["voted_ksm"],
+        mode="lines+markers",
+        line=dict(color="rgb(0, 0, 100)"),
+        marker=dict(color="rgb(0, 0, 100)", size=8),
+        hovertemplate="Referendum id: %{x:.0f}<br>"
+        + "voted ksm mean: %{y:.4f}<br>"
+        + "<extra></extra>",
+    ),
+    go.Scatter(
+        name="median",
+        x=df_voted_ksm_median.index,
+        y=df_voted_ksm_median["voted_ksm"],
+        mode="lines+markers",
+        line=dict(color="rgb(0, 200, 200)"),
+        marker=dict(color="rgb(0, 200, 200)", size=8),
+        hovertemplate="Referendum id: %{x:.0f}<br>"
+        + "voted ksm median: %{y:.4f}<br>"
+        + "<extra></extra>",
+    ),
+]
+
+forth_graph_layout_1 = go.Layout(
+    title="<b>Conviction Mean and Median for selected Referendum IDs</b>",
+    paper_bgcolor="rgb(248, 248, 255)",
+    plot_bgcolor="rgb(248, 248, 255)",
+    xaxis=dict(title="Referendum ID", linecolor="#BCCCDC"),
+    yaxis=dict(title="Conviction", linecolor="#021C1E"),
+    legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01),
+)
+
+forth_graph_layout_2 = go.Layout(
+    title="<b>Voted KSM Mean and Median for selected Referendum IDs</b>",
+    paper_bgcolor="rgb(248, 248, 255)",
+    plot_bgcolor="rgb(248, 248, 255)",
+    xaxis=dict(title="Referendum ID", linecolor="#BCCCDC"),
+    yaxis=dict(title="Voted KSM", linecolor="#021C1E"),
+    legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01),
+)
+
+fig_forth_graph_1 = go.Figure(data=forth_graph_data_1, layout=forth_graph_layout_1)
+fig_forth_graph_2 = go.Figure(data=forth_graph_data_2, layout=forth_graph_layout_2)
+
+col1, col2 = st.columns([1, 1])
+for (col, fig) in zip([col1, col2], [fig_forth_graph_1, fig_forth_graph_2]):
+    with col:
+        st.plotly_chart(fig, use_container_width=True)
+
+
+# Fifth chart
+df_locked_amount_mean = votes_df[["id", "locked_amount"]].groupby("id").mean()
+df_locked_amount_median = votes_df[["id", "locked_amount"]].groupby("id").median()
+df_locked_amount_75_quantile = (
+    votes_df[["id", "locked_amount"]].groupby("id").quantile(0.75)
+)
+
+
+fifth_graph_data = [
+    go.Scatter(
+        name="mean",
+        x=df_locked_amount_mean.index,
+        y=df_locked_amount_mean["locked_amount"],
+        mode="lines+markers",
+        line=dict(color="rgb(0, 0, 100)", dash='dot'),
+        marker=dict(color="rgb(0, 0, 100)", size=6),
+        hovertemplate="Referendum id: %{x:.0f}<br>"
+        + "locked amount mean: %{y:.4f}<br>"
+        + "<extra></extra>",
+    ),
+    go.Scatter(
+        name="median",
+        x=df_locked_amount_median.index,
+        y=df_locked_amount_median["locked_amount"],
+        yaxis="y2",
+        mode="lines+markers",
+        line=dict(color="rgb(0, 200, 200)", dash='dash'),
+        marker=dict(color="rgb(0, 200, 200)", size=6),
+        hovertemplate="Referendum id: %{x:.0f}<br>"
+        + "locked amount median: %{y:.4f}<br>"
+        + "<extra></extra>",
+    ),
+    go.Scatter(
+        name="75th quantile",
+        x=df_locked_amount_75_quantile.index,
+        y=df_locked_amount_75_quantile["locked_amount"],
+        yaxis="y2",
+        mode="lines+markers",
+        line=dict(color="rgb(255, 53, 184)"),
+        marker=dict(color="rgb(255, 53, 184)", size=6, symbol="square"),
+        hovertemplate="Referendum id: %{x:.0f}<br>"
+        + "locked amount 75th quantile: %{y:.4f}<br>"
+        + "<extra></extra>",
+    ),
+]
+
+fifth_graph_layout = go.Layout(
+    title="<b>Locked KSM Mean and Median for selected Referendum IDs</b>",
+    paper_bgcolor="rgb(248, 248, 255)",
+    plot_bgcolor="rgb(248, 248, 255)",
+    xaxis=dict(title="Referendum ID", linecolor="#BCCCDC"),
+    yaxis=dict(title="Locked KSM - Mean", linecolor="#021C1E"),
+    yaxis2=dict(
+        title="Locked KSM - Median & Quantile",
+        linecolor="#021C1E",
+        anchor="x",
+        overlaying="y",
+        side="right",
+    ),
+    legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01),
+)
+
+fig_fifth_graph = go.Figure(data=fifth_graph_data, layout=fifth_graph_layout)
+st.plotly_chart(fig_fifth_graph)
 
 
 ## Table
