d44ab4678a9d5aad6d64e1fa3597570b590d27bb
claudiazi <claudiazi1244@gmail.com>
2022-09-06 21:00:25 +0200

    feature: function data_perc_bars

---
+++ utils/plotting.py
@@ -0,0 +1,34 @@
+def data_perc_bars(df, column):
+    n_bins = 100
+    bounds = [i * (1.0 / n_bins) for i in range(n_bins + 1)]
+    ranges = [
+        ((df[column].max() - df[column].min()) * i) + df[column].min()
+        for i in bounds
+    ]
+    styles = []
+    for i in range(1, len(bounds)):
+        min_bound = ranges[i - 1]
+        max_bound = ranges[i]
+        max_bound_percentage = 100
+        styles.append({
+            'if': {
+                'filter_query': (
+                    '{{{column}}} >= {min_bound}' +
+                    (' && {{{column}}} < {max_bound}' if (i < len(bounds) - 1) else '')
+                ).format(column=column, min_bound=min_bound, max_bound=max_bound),
+                'column_id': column
+            },
+            'background': (
+                """
+                    linear-gradient(90deg,
+                    #3D9970 0%,
+                    #3D9970 {max_bound_percentage}%,
+                    #FF4136 {max_bound_percentage}%,
+                    #FF4136 100%)
+                """.format(max_bound_percentage=max_bound_percentage)
+            ),
+            'paddingBottom': 2,
+            'paddingTop': 2
+        })
+
+    return styles
