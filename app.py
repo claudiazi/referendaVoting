3b25e5ad2dd5f10716b9b72b0f80075df7fa935b
claudiazi <claudiazi1244@gmail.com>
2022-12-19 20:47:10 +0800

    enhancement: clean app.py

--- app.py
+++ app.py
@@ -15,28 +15,4 @@ app = dash.Dash(
 server = app.server
 app.config["suppress_callback_exceptions"] = True
 
-
-mongodb_url = os.getenv("MONGODB_URL")
-db_name = os.getenv("DB_NAME")
-table_name = os.getenv("TABLE_NAME")
-
-suffix_row = "_row"
-suffix_button_id = "_button"
-suffix_sparkline_graph = "_sparkline_graph"
-suffix_count = "_count"
-suffix_ooc_n = "_OOC_number"
-suffix_ooc_g = "_OOC_graph"
-suffix_indicator = "_indicator"
-
-app_color = {"graph_bg": "#082255", "graph_line": "#007ACE"}
-
-theme = {
-    "dark": True,
-    "detail": "#2d3038",  # Background-card
-    "primary": "#007439",  # Green
-    "secondary": "#FFD15F",  # Accent
-}
-
-# read from subsquid endpoint
-
 cache = Cache(app.server, config={"CACHE_TYPE": "filesystem", "CACHE_DIR": "./cache"})
