7d550f0289877cf56d2f6fb354452501b6862513
claudiazi <claudiazi1244@gmail.com>
2023-03-25 18:04:11 +0100

    feat: adjust index

--- index.py
+++ index.py
@@ -1,20 +1,16 @@
-import json
-import time
 import warnings
 
-import pandas as pd
-import requests
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
+
+from app import app
+from tabs import tab_1, tab_2, tab_3, gov2_tab_1, gov2_tab_2
 from utils.data_preparation import (
     load_current_block,
     load_referenda_stats_gov1,
     load_referenda_stats_gov2,
 )
-
-from app import app
-from tabs import tab_1, tab_2, tab_3, gov2_tab_1
 
 
 def build_banner():
@@ -29,7 +25,7 @@ def build_banner():
             html.Div(
                 className="two columns",
                 children=dcc.Dropdown(
-                    ["Gov 1", "Gov 2"], value="Gov 1", id="gov_version"
+                    ["Gov 1", "Gov 2"], value="Gov 2", id="gov_version"
                 ),
             ),
         ],
@@ -104,7 +100,7 @@ app.layout = html.Div(
         build_banner(),
         dcc.Interval(
             id="interval-component",
-            interval=2 * 1000,  # in milliseconds
+            interval=5 * 1000,  # in milliseconds
             n_intervals=0,
             disabled=True,
         ),
@@ -164,13 +160,17 @@ def update_historical_data(n_intervals, gov_version):
     [Input("app-tabs", "value"), Input("gov_version", "value")],
 )
 def render_tab_content(tab_switch, gov_version):
+    if tab_switch == "tab1" and gov_version == "Gov 2":
+        return gov2_tab_1.layout
+    if tab_switch == "tab2" and gov_version == "Gov 2":
+        return gov2_tab_2.layout
     if tab_switch == "tab1" and gov_version == "Gov 1":
         return tab_1.layout
     if tab_switch == "tab2" and gov_version == "Gov 1":
         return tab_2.layout
     if tab_switch == "tab3" and gov_version == "Gov 1":
         return tab_3.layout
-    return gov2_tab_1.layout
+    return
 
 
 
